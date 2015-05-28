f679c74732bf82fe8549ecce7ad5293e93f801e7
YongkyunLee <ykhl1itj@naver.com>
2015-05-28 20:31:42 +0900

    Mergesort_ver2.0
    importing integer text file function added
    inversion counting function added

--- meregesort_practice.py
+++ meregesort_practice.py
@@ -1,19 +1,20 @@
 import math
 
-input_list = [1, 8, 13, 5, 3, 9, 10, 11, 7, 6, 2, 4, 12]
 sort_list = []
-operation_num = math.ceil(math.log2(len(input_list)))
-#print(operation_num)
-for i in range(0, operation_num+1):
-    sort_list.insert(i, [])
-#print(sort_list)
-for i in range(0, len(input_list)):
-    sort_list[0].insert(i, [input_list[i]])
-#print(sort_list)
-    
+
+def int_list(file):
+    integer_list = []
+    text = open(file)
+    text_read = text.read()
+    text_list = text_read.splitlines()
+    for i in range(0, len(text_list)):
+        integer_list.insert(i, int(text_list[i]))
+    return integer_list
+
 def merge(left_list, right_list):
     i = 0
     j = 0
+    add_count = 0
     merge_list = []
     for k in range(0, len(left_list)+len(right_list)):
         if i == len(left_list):
@@ -31,13 +32,27 @@ def merge(left_list, right_list):
         if left_list[i] > right_list[j] and i < len(left_list) and j < len(right_list):
             merge_list.insert(k, right_list[j])
             j += 1
+            add_count += len(left_list)-i
             continue
-    return merge_list
+    return [merge_list, add_count]
+
+inv_count = 0
+input_list = int_list('c:\\IntegerArray.txt')
+#print(input_list)
+operation_num = math.ceil(math.log2(len(input_list)))
+for i in range(0, operation_num+1):
+    sort_list.insert(i, [])
+for i in range(0, len(input_list)):
+    sort_list[0].insert(i, [input_list[i]])
 
 for i in range(0, operation_num):
-    for x in range(0, math.floor(len(sort_list[i])/2)):
-        sort_list[i+1].insert(x, merge(sort_list[i][x*2], sort_list[i][x*2+1]))
+    mid = len(sort_list[i])/2
+    for x in range(0, math.floor(mid)):
+        sort_list[i+1].insert(x, merge(sort_list[i][x*2], sort_list[i][x*2+1])[0])
+        inv_count += merge(sort_list[i][x*2], sort_list[i][x*2+1])[1]
     if len(sort_list[i])%2 == 1:
-        sort_list[i+1].insert(math.ceil(len(sort_list[i])/2)-1, sort_list[i][len(sort_list[i])-1])
-    #print(sort_list[i])
-print(sort_list[operation_num])
+        sort_list[i+1].insert(math.ceil(mid)-1, sort_list[i][len(sort_list[i])-1])
+#    print(inv_count)
+
+#print(sort_list[operation_num][0])
+print(inv_count)
